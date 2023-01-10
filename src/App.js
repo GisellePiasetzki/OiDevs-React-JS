1c2dbd305cf1680a21be00f9a98fd5e2d9f45f97
GisellePiasetzki <piasetzki.giselle@gmail.com>
2023-01-10 16:23:52 -0300

    iniciando projeto com reactjs

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Giselle Piasetzki - iniciando com JS
         </a>
       </header>
     </div>
